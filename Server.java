e079f1a1d5a4c8801adea34e3c732fdc1683545f
tebelloSekhoto <tebello.scdc@gmail.com>
2019-10-08 12:06:29 +0200

    defined server socket

---
+++ Server.java
@@ -0,0 +1,16 @@
+// The skeleton program for Server
+import java.net.*;
+import java.io.*;
+
+public class Server
+{
+	private Socket socket = null;
+	private ServerSocket server = null;
+	private DataInputStream in  = null;
+}
+
+    public static void main(String args[])
+    {
+        Server server = new Server(5000);
+    }
+}
